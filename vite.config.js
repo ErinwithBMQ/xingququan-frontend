e8672518857d3849e52b95b9aae4d14c40782467
ErinwithBMQ <631165975@qq.com>
2024-08-10 15:00:50 +0800

    8.10 部署

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                login: 'login.html',
+                mycomment: 'mycomment.html',
+                mylike: 'mylike.html',
+                mypost: 'mypost.html',
+                others_comment: 'others_comment.html',
+                others_like: 'others_like.html',
+                register: 'register.html',
+                xqq_choose: 'xqq_choose.html',
+                xqq_create: 'xqq_create.html'
+            }
+        }
+    }
 })
